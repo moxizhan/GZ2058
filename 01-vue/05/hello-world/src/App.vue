71ddc1cf0d3463bf3ad9f6a70a9e441e79b6ae46
Nicholas <nicholas@example.com>
2020-12-04 17:16:13 +0800

    事件修饰符&表单&key&cli

---
+++ 01-vue/05/hello-world/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Hello</h1>
+    <Test></Test>
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 01-vue/05/vue-cli.md
@@ -0,0 +1,10 @@
+npm install -g @vue/cli
+
+
+vue --version
+
+vue create hello-world
+vue2 
+
+ $ cd hello-world
+ $ npm run serve
