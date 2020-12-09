087fac99d4373b28dbe205a96dad15642a4bd185
Nicholas <nicholas@example.com>
2020-12-09 15:08:25 +0800

    路由组件传参&vueCli预设

---
+++ 02-vue-router/02-router/router-test/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about?age=17">About</router-link>
+    </div>
+    <hr />
+    <User id="nicholas" />
+
+    <hr />
+
+
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import User from '@/components/User.vue'
+export default {
+  components:{
+    User
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ 02-vue-router/02-router/router-test/src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <div> User -- {{id}}</div>
+</template>
+
+<script>
+export default {
+    props: ['id']
+}
+</script>
+
+<style>
+
+</style>
