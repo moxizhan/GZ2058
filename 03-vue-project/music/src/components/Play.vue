a191e771d7faff466b24cb521c66ec88fe87cdf3
Nicholas <nicholas@example.com>
2020-12-16 15:56:07 +0800

    播放进度条

--- 03-vue-project/music/src/components/Play.vue
+++ 03-vue-project/music/src/components/Play.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="paly">
+  <div class="play" :class="{ paused: paused }">
     <audio
       :src="
         'https://music.163.com/song/media/outer/url?id=' +
         currentMusic.id +
         '.mp3'
       "
-      controls
       autoplay
-      style="width: 100%; height: 40px"
       ref="audio"
     ></audio>
 
@@ -17,8 +15,19 @@
       enter-active-class="animate__animated animate__slideInUp"
       leave-active-class="animate__animated animate__slideOutDown"
     >
-      <div class="play-bar" v-if="isShowPlayBar" @click="isShowPlayBar = false">
-        <div>{{ currentMusic.name }}</div>
+      <div
+        class="play-bar"
+        v-show="isShowPlayBar"
+        @click="isShowPlayBar = false"
+      >
+        <img :src="currentMusic.picUrl" alt="" />
+        <h5>
+          {{ currentMusic.name }}
+        </h5>
+        <div class="control" @click.stop="togglePlayState">
+          <canvas ref="circle" width="50" height="50"></canvas>
+          <span class="icon"></span>
+        </div>
       </div>
     </transition>
 
@@ -39,7 +48,11 @@
             @toggle-show-lyric="isShowLyric = !isShowLyric"
           />
         </template>
-        <PlayFullFooter />
+        <PlayFullFooter
+          v-bind:currentTime="currentTime"
+          v-bind:duration="duration"
+          @update:currentTime="$refs.audio.currentTime = $event"
+        />
       </div>
     </transition>
   </div>
@@ -61,6 +74,8 @@ export default {
   data: function () {
     return {
       paused: null,
+      duration: 0,
+      currentTime: 0,
       isShowLyric: false,
       isShowPlayBar: true,
     };
@@ -72,19 +87,68 @@ export default {
   mounted() {
     console.log("mounted", this.$refs.audio);
     // console.log(this.$refs.usernameInput);
-
-    this.$refs.audio.addEventListener("pause", () => {
+    let audio = this.$refs.audio;
+    audio.addEventListener("pause", () => {
       console.log("暂停");
       this.paused = true;
     });
-    this.$refs.audio.addEventListener("play", () => {
+    audio.addEventListener("play", () => {
       console.log("播放");
       this.paused = false;
+    });
+
+    // var _this = this
+    audio.addEventListener("durationchange", () => {
+      // console.log("当前歌曲时长", this.duration);
+      this.duration = audio.duration;
+    });
+
+    audio.addEventListener("timeupdate", () => {
+      // console.log("当前播放到", this.currentTime);
+      this.currentTime = audio.currentTime;
+
+      this.drawCircle(this.currentTime, this.duration);
     });
   },
   // updated() {
   //   console.log("update", this.$refs.usernameInput);
   // },
+
+  methods: {
+    drawCircle: function (n, total) {
+      let canvas = this.$refs.circle;
+      let ctx = canvas.getContext("2d");
+
+      ctx.clearRect(0, 0, 50, 50);
+      ctx.beginPath();
+      ctx.strokeStyle = "rgba(0, 0, 0, 0.3)";
+      ctx.arc(25, 25, 20, 0, Math.PI * 2, false); // 绘制
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.strokeStyle = "rgba(255, 0, 0, 0.6)";
+      ctx.arc(
+        25,
+        25,
+        19,
+        Math.PI * -0.5,
+        Math.PI * ((n / total) * 2 - 0.5),
+        false
+      ); // 绘制
+      ctx.stroke();
+      ctx.closePath();
+    },
+
+    togglePlayState: function () {
+      let audio = this.$refs.audio;
+      if (this.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    },
+  },
 
   watch: {
     paused: function (n) {
@@ -94,13 +158,58 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="less">
+.play {
+  &.paused {
+    .play-bar {
+      img {
+        animation-play-state: paused;
+      }
+      .control {
+        span.icon {
+          &::before {
+            content: "播放";
+          }
+        }
+      }
+    }
+  }
+}
 .play-bar {
   position: fixed;
   bottom: 0;
   left: 0;
+  z-index: 9;
   width: 100%;
-  background: green;
+  background: white;
+  display: flex;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.15);
+  img {
+    width: 36px;
+    height: 36px;
+    margin: 7px;
+    border-radius: 50%;
+    animation: playing 6s linear infinite;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.15);
+  }
+  h5 {
+    flex: 1;
+  }
+  .control {
+    position: relative;
+    span.icon {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      // display: block;
+      // background: rgba(240, 128, 128, 0.507);
+      &::before {
+        content: "暂停";
+      }
+    }
+  }
 }
 .play-full {
   background: rgba(255, 0, 0, 1);
@@ -110,5 +219,14 @@ export default {
   top: 0;
   left: 0;
   z-index: 9;
+}
+
+@keyframes playing {
+  form {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
--- 03-vue-project/music/src/components/PlayFullFooter.vue
+++ 03-vue-project/music/src/components/PlayFullFooter.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="play-full-footer">PlayFullFooter</div>
+  <div class="play-full-footer">
+    <input type="range" min="0" :max="duration" step="1" @input="getValue" :value="currentTime" />
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["currentTime", "duration"],
+  data: function () {
+    return {};
+  },
+  methods: {
+    getValue: function (e) {
+      console.log(e.target.value);
+      this.$emit('update:currentTime', e.target.value)
+    }
+  },
+};
 </script>
 
 <style lang="less">
