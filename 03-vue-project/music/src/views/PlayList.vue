d448690d7066b6df11b7397c6d715575a4612847
Nicholas <nicholas@example.com>
2020-12-23 17:31:35 +0800

    音乐搜索

--- 03-vue-project/music/src/views/PlayList.vue
+++ 03-vue-project/music/src/views/PlayList.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="playlist">
-      playlist
-  </div>
+  <div class="playlist">playlist</div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id: null,
+    };
+  },
+  created() {
+    // console.log('====================================');
+    // console.log(this.$route.query.id);
+    // console.log('====================================');
+    this.id = this.$route.query.id
+  },
 
-}
+  watch: {
+    // "$route.query.id": function (n) {
+    //   // console.log('watch',n);
+    //   this.id = n
+    // },
+    id: function () {
+      console.log('id', this.id);
+    }
+  },
+
+  beforeRouteUpdate(to, from, next) {
+    // console.log('beforeUpdate',to.query.id);
+    this.id = to.query.id
+    next();
+  },
+  activated() {
+    // console.log('activated',this.$route.query.id);
+    this.id = this.$route.query.id
+  },
+};
 </script>
 
 <style>
-
 </style>
--- 03-vue-project/music/src/views/Search.vue
+++ 03-vue-project/music/src/views/Search.vue
@@ -1,15 +1,92 @@
 <template>
   <div class="search">
-      search
+    <input
+      type="text"
+      @input="inputing = true"
+      @keyup.enter="inputing = false"
+      v-model.trim="inputValue"
+    />
+    <button @click="inputValue = ''">X</button>
+
+    <hr />
+    <!-- <button @click="inputValue = '迪迦奥特曼'" v-if="!inputing">
+      迪迦奥特曼
+    </button>
+    <template v-else>
+      <ul v-if="inputing">
+        <li
+          @click="
+            inputValue = item.keyword;
+            inputing = false;
+          "
+          v-for="(item, index) in suggests"
+          :key="index"
+        >
+          {{ item.keyword }}
+        </li>
+      </ul>
+      <ul v-else>
+        <li>搜索结果</li>
+      </ul>
+    </template> -->
+    <button
+      @click="inputValue = '迪迦奥特曼'"
+      v-if="!suggests.length && !inputing && !inputValue"
+    >
+      迪迦奥特曼
+    </button>
+
+    <ul v-if="inputing && suggests.length">
+      <li
+        @click="
+          inputValue = item.keyword;
+          inputing = false;
+        "
+        v-for="(item, index) in suggests"
+        :key="index"
+      >
+        {{ item.keyword }}
+      </li>
+    </ul>
+
+    <ul v-if="!inputing && inputValue">
+      <li>搜索结果</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      inputValue: "",
+      suggests: [],
+      inputing: false,
+    };
+  },
 
-}
+  watch: {
+    inputValue: function (n) {
+      if (n) {
+        if (this.inputing) {
+          this.axios
+            .get("/search/suggest?keywords=" + n + "&type=mobile")
+            .then((res) => {
+              console.log("搜索建议", res.data.result.allMatch);
+              this.suggests = res.data.result.allMatch;
+            });
+        } else {
+          console.log("搜索结果");
+          // this.results = true
+        }
+      } else {
+        this.inputing = false;
+        this.suggests = [];
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
